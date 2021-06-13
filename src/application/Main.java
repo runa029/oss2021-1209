8767e290fcfe1f446d0f600a19f31ba859c66fea
CSJ <tmdwns9630@gmail.com>
2021-06-13 21:53:22 +0900

    커밋 테스트

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-
+//clone test
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
